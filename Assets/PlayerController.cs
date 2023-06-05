1a4563ce811b3098763fa18e5468de001d44c5dd
Alissen <alissen.moreno61@yahoo.com>
2023-06-05 14:13:40 -0500

    Game Reuploaded So we had created a game, however we kept running into issues since the gitignore file wasn't being taken into consideration. This meant that the entire library would be pushed into github. Our stuff started disappearing so Tomai recommended we first fix the library issue by doing a new one. And well... here we are lol

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,225 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // public float speed;
+    // public Camera cam;
+    // private Vector3 lastMousePosition;
+
+    // void Start()
+    // {
+    //     lastMousePosition = Input.mousePosition;
+    // }
+
+    // void Update()
+    // {
+    //     // Check if the mouse has moved
+    //     if (Input.mousePosition != lastMousePosition)
+    //     {
+    //         // Update the last mouse position
+    //         lastMousePosition = Input.mousePosition;
+
+    //         // Cast a ray from the mouse position into the world
+    //         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+    //         RaycastHit hit;
+
+    //         if (Physics.Raycast(ray, out hit))
+    //         {
+    //             // Get the position on the ground where the ray hit
+    //             Vector3 targetPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+
+    //             // Calculate the direction from the player's current position to the target position
+    //             Vector3 direction = targetPos - transform.position;
+
+    //             // Normalize the direction vector to get a unit direction
+    //             direction.Normalize();
+
+    //             // Calculate the target position based on the direction and speed
+    //             Vector3 moveTarget = transform.position + direction * speed * Time.deltaTime;
+
+    //             // Move the player towards the target position
+    //             transform.position = moveTarget;
+    //         }
+    //     }
+    // }
+// public float speed;
+// public Camera cam;
+// private Vector3 lastMousePosition;
+// private Animator animator;
+
+// void Start()
+// {
+//     lastMousePosition = Input.mousePosition;
+//     animator = GetComponent<Animator>();
+// }
+
+// void Update()
+// {
+//     // Check if the mouse has moved
+//     if (Input.mousePosition != lastMousePosition)
+//     {
+//         // Update the last mouse position
+//         lastMousePosition = Input.mousePosition;
+
+//         // Cast a ray from the mouse position into the world
+//         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+//         RaycastHit hit;
+
+//         if (Physics.Raycast(ray, out hit))
+//         {
+//             // Get the position on the ground where the ray hit
+//             Vector3 targetPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+
+//             // Calculate the direction from the player's current position to the target position
+//             Vector3 direction = targetPos - transform.position;
+
+//             // Normalize the direction vector to get a unit direction
+//             direction.Normalize();
+
+//             // Calculate the target position based on the direction and speed
+//             Vector3 moveTarget = transform.position + direction * speed * Time.deltaTime;
+
+//             // Move the player towards the target position
+//             transform.position = moveTarget;
+
+//             // Set the IsRunning parameter in the animator to true
+//             animator.SetBool("isRunning", true);
+//         }
+//         else
+//         {
+//             // Set the IsRunning parameter in the animator to false
+//             animator.SetBool("isRunning", false);
+//         }
+//     }
+// }
+
+// public float speed;
+// public Camera cam;
+// private Vector3 lastMousePosition;
+// private Animator animator;
+
+// void Start()
+// {
+//     lastMousePosition = Input.mousePosition;
+//     animator = GetComponent<Animator>();
+// }
+
+// void Update()
+// {
+//     // Check if the left mouse button is clicked
+//     if (Input.GetMouseButtonDown(0))
+//     {
+//         // Change the animation to "Attack"
+//         animator.SetTrigger("isAttacking");
+//     }
+
+//     // Check if the mouse has moved
+//     if (Input.mousePosition != lastMousePosition)
+//     {
+//         // Update the last mouse position
+//         lastMousePosition = Input.mousePosition;
+
+//         // Cast a ray from the mouse position into the world
+//         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+//         RaycastHit hit;
+
+//         if (Physics.Raycast(ray, out hit))
+//         {
+//             // Get the position on the ground where the ray hit
+//             Vector3 targetPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+
+//             // Calculate the direction from the player's current position to the target position
+//             Vector3 direction = targetPos - transform.position;
+
+//             // Normalize the direction vector to get a unit direction
+//             direction.Normalize();
+
+//             // Calculate the target position based on the direction and speed
+//             Vector3 moveTarget = transform.position + direction * speed * Time.deltaTime;
+
+//             // Move the player towards the target position
+//             transform.position = moveTarget;
+
+//             // Set the IsRunning parameter in the animator to true
+//             animator.SetBool("isRunning", true);
+//         }
+//         else
+//         {
+//             // Set the IsRunning parameter in the animator to false
+//             animator.SetBool("isRunning", false);
+//         }
+//     }
+// }
+public float speed;
+public Camera cam;
+private Vector3 lastMousePosition;
+public GameObject prefab; // Reference to the prefab with Animator component
+private Animator playerAnimator; // Animator component of the player
+
+void Start()
+{
+    lastMousePosition = Input.mousePosition;
+    playerAnimator = GetComponent<Animator>(); // Get the Animator component of the player
+}
+
+void Update()
+{
+    // Check if the left mouse button is clicked
+    if (Input.GetMouseButtonDown(0))
+    {
+        // Trigger the "isAttacking" animation on the player
+        playerAnimator.SetTrigger("isAttacking");
+
+        // Instantiate the prefab at the player's position
+        GameObject instance = Instantiate(prefab, transform.position, transform.rotation);
+
+        // Get the Animator component from the instantiated prefab
+        Animator prefabAnimator = instance.GetComponent<Animator>();
+
+        // Trigger the "isAttacking" animation on the prefab
+        if (prefabAnimator != null)
+        {
+            prefabAnimator.SetTrigger("isAttacking");
+        }
+    }
+
+    // Check if the mouse has moved
+   if (Input.mousePosition != lastMousePosition)
+    {
+        // Update the last mouse position
+        lastMousePosition = Input.mousePosition;
+
+        // Cast a ray from the mouse position into the world
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Get the position on the ground where the ray hit
+            Vector3 targetPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+
+            // Calculate the direction from the player's current position to the target position
+            Vector3 direction = targetPos - transform.position;
+
+            // Normalize the direction vector to get a unit direction
+            direction.Normalize();
+
+            // Calculate the target position based on the direction and speed
+            Vector3 moveTarget = transform.position + direction * speed * Time.deltaTime;
+
+            // Move the player towards the target position
+            transform.position = moveTarget;
+
+            // Set the IsRunning parameter in the animator to true
+            playerAnimator.SetBool("isRunning", true);
+        }
+        else
+        {
+            // Set the IsRunning parameter in the animator to false
+            playerAnimator.SetBool("isRunning", false);
+        }
+    }
+}
+}
---
+++ Assets/PlayerHealthBar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+
+	public int maxHealth = 100;
+	public int currentHealth;
+
+	public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+
+	 public void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+
+		healthBar.SetHealth(currentHealth);
+
+		if(currentHealth<=0)
+		{
+		Destroy(gameObject);	
+		 #if UNITY_EDITOR
+               UnityEditor.EditorApplication.isPlaying = false;
+               #else
+                   Application.Quit();
+               #endif
+           currentHealth=maxHealth; //make sure to update 
+           healthBar.SetHealth(currentHealth);
+		}
+
+	}
+	
+
+	
+}
